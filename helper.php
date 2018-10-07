c006426245901dec84f7e4edcd51dc41e54e4f87
wedal <wedal@wedal.ru>
2018-10-07 22:21:27 +0600

    quick fix

--- helper.php
+++ helper.php
@@ -19,7 +19,6 @@ class ModWedalJoomlaCallbackHelper
 
 	public static function getFormAjax()
 	{
-		///index.php?option=com_ajax&module=wedal_joomla_callback&format=raw&method=getForm
 		$params = ModWedalJoomlaCallbackHelper::getParams();
 		require JModuleHelper::getLayoutPath('mod_wedal_joomla_callback', $params->get('layout', 'default') . '_form');
 	    return;
