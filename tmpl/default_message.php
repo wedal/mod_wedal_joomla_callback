f8a9b370e6e1a99258650b44f7290ee7ead31084
wedal <wedal@wedal.ru>
2020-04-03 22:05:56 +0600

    Notice Ajax Error fix

--- tmpl/default_message.php
+++ tmpl/default_message.php
@@ -2,31 +2,31 @@
 
 <div class="message">
 
-    <?php if ($formfields->name['show']) { ?>
+    <?php if (!empty($formfields->name['show'])) { ?>
         <div>
             <span><?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_NAME'); ?></span>: <span><?php echo $formfields->name['value']; ?></span>
         </div>
     <?php } ?>
 
-    <?php if ($formfields->email['show']) { ?>
+    <?php if (!empty($formfields->email['show'])) { ?>
         <div>
             <span><?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_MAIL'); ?></span>: <span><?php echo $formfields->email['value']; ?></span>
         </div>
     <?php } ?>
 
-    <?php if ($formfields->phone['show']) { ?>
+    <?php if (!empty($formfields->phone['show'])) { ?>
         <div>
             <span><?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_PHONE'); ?></span>: <span><?php echo $formfields->phone['value']; ?></span>
         </div>
     <?php } ?>
 
-    <?php if ($formfields->comment['show']) { ?>
+    <?php if (!empty($formfields->comment['show'])) { ?>
         <div>
             <span><?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_TEXTAREA'); ?></span>: <span><?php echo $formfields->comment['value']; ?></span>
         </div>
     <?php } ?>
 
-    <?php if ($page_url) { ?>
+    <?php if (!empty($page_url)) { ?>
         <div><?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_SEND_FROM_URL').$page_url ?></div>
     <?php } ?>
 </div>
--- tmpl/default_popupform.php
+++ tmpl/default_popupform.php
@@ -7,7 +7,7 @@
 
         		<div class="modal-header">
         			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <?php if ($formtitle) { ?>
+                    <?php if (!empty($formtitle)) { ?>
                         <div class="form-header">
                             <h2 class="modal-title"><?php echo $formtitle ?></h2>
                         </div>
@@ -16,44 +16,44 @@
 
         		<div class="modal-body">
 
-                    <?php if ($formdesc) { ?>
+                    <?php if (!empty($formdesc)) { ?>
             			<div class="informtext one-click-desc">
             				<?php echo $formdesc; ?>
             			</div>
                     <?php } ?>
 
-                    <?php if ($formfields->name['show']) { ?>
+                    <?php if (!empty($formfields->name['show'])) { ?>
             			<div class="inputcont">
             				<input type="text" placeholder="<?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_NAME'); ?> <?php echo $formfields->name['req'][0] ?>" value="" class="inputbox <?php echo $formfields->name['req'][1] ?> form-control" data-error="<?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_NAME_ERROR');  ?>" id="WJCForm<?php echo $moduleId ?>_name" name="WJCForm<?php echo $moduleId ?>_name" />
             			</div>
                     <?php } ?>
 
-                    <?php if ($formfields->phone['show']) { ?>
+                    <?php if (!empty($formfields->phone['show'])) { ?>
             			<div class="inputcont">
             				<input type="text" placeholder="<?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_PHONE'); ?> <?php echo $formfields->phone['req'][0] ?>" value="" class="inputbox <?php echo $formfields->phone['req'][1] ?> form-control" data-error="<?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_PHONE_ERROR');  ?>" id="WJCForm<?php echo $moduleId ?>_phone" name="WJCForm<?php echo $moduleId ?>_phone" />
             			</div>
                     <?php } ?>
 
-                    <?php if ($formfields->email['show']) { ?>
+                    <?php if (!empty($formfields->email['show'])) { ?>
                         <div class="inputcont">
                             <input type="text" placeholder="<?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_MAIL'); ?> <?php echo $formfields->email['req'][0] ?>" value="" class="inputbox <?php echo $formfields->email['req'][1] ?> form-control" data-error="<?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_EMAIL_ERROR');  ?>" id="WJCForm<?php echo $moduleId ?>_email" name="WJCForm<?php echo $moduleId ?>_email" />
                         </div>
                     <?php } ?>
 
-                    <?php if ($formfields->comment['show']) { ?>
+                    <?php if (!empty($formfields->comment['show'])) { ?>
                         <div class="inputcont">
                             <textarea id="WJCForm<?php echo $moduleId ?>_comment"  rows="4" cols="10" placeholder="<?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_TEXTAREA'); ?> <?php echo $formfields->comment['req'][0] ?>" data-error="<?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_MESSAGE_ERROR');  ?>" name="WJCForm<?php echo $moduleId ?>_comment" class="customer-comment <?php echo $formfields->comment['req'][1] ?>"></textarea>
                         </div>
                     <?php } ?>
 
-                    <?php if ($formfields->tos['show']) { ?>
+                    <?php if (!empty($formfields->tos['show'])) { ?>
                         <div class="inputcont">
                             <label id="WJCForm<?php echo $moduleId ?>_tos" class="tos">
-                                <?php if ($formfields->tos['toscheckbox']) { ?>
+                                <?php if (!empty($formfields->tos['toscheckbox'])) { ?>
                                     <input id="WJCForm<?php echo $moduleId ?>_tos_box" type="checkbox" class="tos-box required" name="tos_box" value="1" data-error="<?php echo JText::_('MOD_WEDAL_JOOMLA_CALLBACK_TOS_ERROR');  ?>">
                                 <?php } ?>
                                 <span><?php echo JText::sprintf('MOD_WEDAL_JOOMLA_CALLBACK_TOSTEXT', $formfields->tos['toslink'], $formfields->tos['toslinktext']); ?></span>
-                                <?php if ($formfields->tos['toscheckbox']) { ?>
+                                <?php if (!empty($formfields->tos['toscheckbox'])) { ?>
                                     <span>*</span>
                                 <?php } ?>
                             </label>
@@ -71,7 +71,7 @@
     </div>
 </div>
 
-<?php if ($params->get('showphonemask')) {
+<?php if (!empty($params->get('showphonemask'))) {
     echo '
     <script src="/modules/mod_wedal_joomla_callback/assets/js/jquery.maskedinput.min.js" charset="utf-8"></script>
     <script type="text/javascript">
