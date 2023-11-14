f15061514fd1688a9ec6e5397ce9a94fcf1efbf9
wedal <wedal@wedal.ru>
2023-11-14 18:31:42 +0600

    Убран класс btn, т.к. чаще мешает, чем помогает. При желании может быть добавлен в настройках модуля.

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,5 +3,5 @@ defined('_JEXEC') or die('Restricted access');
 ?>
 
 <?php if (!$params->get('hideformbutton')) { ?>
-    <a data-id="<?php echo $module->id ?>" class="wjcallback-link btn <?php echo $params->get('button_suffix') ?>" href="#" <?php echo $params->get('ym_popup') ? 'data-ym-aimid="'.$params->get('ym_popup'). '"' : '' ?>><?php echo $form->buttontext ?></a>
+    <a data-id="<?php echo $module->id ?>" class="wjcallback-link <?php echo $params->get('button_suffix') ?>" href="#" <?php echo $params->get('ym_popup') ? 'data-ym-aimid="'.$params->get('ym_popup'). '"' : '' ?>><?php echo $form->buttontext ?></a>
 <?php } ?>
